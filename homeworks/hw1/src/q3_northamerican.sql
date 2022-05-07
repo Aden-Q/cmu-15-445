405290b639172b4f07ee47222ce7b98ee679ed10
aden-q <qian0102@umn.edu>
2022-05-07 00:08:49 -0500

    Update

--- homeworks/hw1/src/q3_northamerican.sql
+++ homeworks/hw1/src/q3_northamerican.sql
@@ -1,8 +1,4 @@
-WITH TempTable AS (
-    SELECT ShipCountry
-)
-
 SELECT Id, ShipCountry
-FROM 'Order', TempTable
+FROM 'Order'
 WHERE ShipCountry IN ('USA', 'Mexico', 'Canada')
 ORDER BY Id ASC LIMIT 20 OFFSET 50;
